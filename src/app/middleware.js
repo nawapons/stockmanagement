357684eb4f3db2ca46ff21ef8e2536b8d6ab9f53
nawapons <nawapon.s@cvc.ac.th>
2024-02-13 19:54:16 +0700

    fix cors

--- src/app/middleware.js
+++ src/app/middleware.js
@@ -1,13 +1,23 @@
-import { NextResponse } from "next/server";
+import { NextResponse } from "next/server"
 
-export function middleware(req) {
-    if (req.method === "OPTIONS") {
-        return NextResponse.json({});
+const corsHeaders = {
+    "Access-Control-Allow-Origin": "*",
+    "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, OPTIONS",
+    "Access-Control-Allow-Headers": "Content-Type, Authorization",
+}
+
+export function middleware(request) {
+    if (request.method === "OPTIONS") {
+        return NextResponse.json({}, { headers: corsHeaders })
     }
+    const response = NextResponse.next()
+    Object.entries(corsHeaders).forEach(([key, value]) => {
+        response.headers.append(key, value)
+    })
 
-    return NextResponse.next();
+    return response
 }
 
 export const config = {
     matcher: "/api/:path*",
-};
+}
